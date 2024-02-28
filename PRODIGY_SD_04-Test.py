7e9d38b636f438a9589e10a1711b0e881ca8fdb6
Rishikesh Verma <66529154+Rishikesh-Verma69@users.noreply.github.com>
2024-02-29 02:52:26 +0530

    Add files via upload

---
+++ PRODIGY_SD_04-Test.py
@@ -0,0 +1,68 @@
+def solve_sudoku(board):
+    empty_cell = find_empty_cell(board)
+    if not empty_cell:
+        return True
+
+    row, col = empty_cell
+    for num in range(1, 10):
+        if valid(board, row, col, num):
+            board[row][col] = num
+            if solve_sudoku(board):
+                return True
+            board[row][col] = 0
+
+    return False
+
+
+def find_empty_cell(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+    return None
+
+
+def valid(board, row, col, num):
+    if num in board[row]:
+        return False
+
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+
+    return True
+
+
+def print_board(board):
+    for row in board:
+        print(row)
+
+
+def main():
+    board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+    if solve_sudoku(board):
+        print("Solution:")
+        print_board(board)
+    else:
+        print("No solution exists.")
+
+
+if __name__ == "__main__":
+    main()
